dfb3ba25e4d6aedc92357641335d4d7c5cc2b34f
Malcolm Kam <malcolm.h.kam@gmail.com>
2024-03-25 13:19:50 -1000

    fixed path to public folder

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/reviews', router);
 
